ac061b2853bef3cd9d5f017ec4a990e920360dd1
rakesh-bhetariya <rakeshbhetariya29@gmail.com>
2024-07-15 08:54:30 +0530

    Create 01_day.js

---
+++ 01_day.js
@@ -0,0 +1,40 @@
+var number = 20;
+console.log(number);
+
+let str = "rakesh";
+console.log(str);
+
+const isthis = true;
+console.log(isthis)
+
+
+let nbr = 24;
+let str2 = "Bhetariya";
+let obj = {
+    firstName : "Rakesh",
+    lastName : "Bhetariya"
+};
+let arr = ['apple', 'banana', 'kiwi'];
+
+console.log(typeof nbr);
+console.log(typeof str);
+console.log(typeof obj);
+console.log(typeof arr);
+
+
+console.log("value :", nbr, "Type of :", typeof nbr);
+console.log("value :", str, "Type of :", typeof str);
+console.log("value :", obj, "Type of :", typeof obj);
+console.log("value :", arr, "Type of :", typeof arr);
+
+
+let fullName = 'Rakesh Bhetariya';
+console.log(fullName);
+
+fullName = 'Rakesh';
+console.log(fullName);
+
+const age = 20;
+console.log(age)
+// age = 20
+// console.log(age)
