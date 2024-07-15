2e6405a5364f024c74c35d9b025805781bd2cb42
rakesh-bhetariya <rakeshbhetariya29@gmail.com>
2024-07-15 21:06:14 +0530

    complete day 2 in 30 days js challenge

---
+++ 02_day.js
@@ -0,0 +1,55 @@
+let nbr1 = 20;
+let nbr2 = 30;
+let nbr3 = 50;
+let nbr4 = 60;
+let nbr5 = 30;
+
+let addnbr = nbr1 + nbr2;
+console.log(addnbr);
+
+let minusnbr = nbr3 - nbr5;
+console.log(minusnbr);
+
+let multipenbr = nbr3 * nbr4;
+let dividenbr = nbr2 / nbr1;
+
+console.log(multipenbr);
+console.log(dividenbr);
+
+console.log(nbr2 += 10)
+console.log(nbr2 -= 10)
+
+if(nbr3 > nbr2){
+    console.log(`${nbr3} is greater than ${nbr2}`)
+}
+
+if(nbr1 < nbr2){
+    console.log(`${nbr1} is less than ${nbr2}`)
+}
+
+if(nbr3 >= nbr5){
+    console.log(`${nbr3} is greater than or equal to ${nbr5}`)
+}
+
+if(nbr1 <= nbr5){
+    console.log(`${nbr1} is less than or equal to ${nbr5}`)
+}
+
+if(nbr3 == 50 && nbr4 == 20){
+    console.log(`${nbr3} is equal to 50 and ${nbr4} is equal to 40`)
+} else {
+    console.log(`${nbr3} is not equal to 50 and ${nbr4} is not equal to 40`)
+}
+
+if(nbr3 == 50 || nbr4 == 20){
+    console.log(`${nbr3} is equal to 50 and ${nbr4} is equal to 40`)
+} else {
+    console.log(`${nbr3} is not equal to 50 and ${nbr4} is not equal to 40`)
+}
+
+if(nbr4 !== 20){
+    console.log(`${nbr4} is not equal to 40`)
+}
+
+
+
