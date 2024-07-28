a7aa6e6cd1bc5b94c2cb4ffcc3bf5ea54718f8d1
rakesh-bhetariya <rakeshbhetariya29@gmail.com>
2024-07-28 18:17:56 +0530

    Update 07_day.js
    while revised day 7 of the 30 days js challenge i realized how many way to iterate that object, because i learned how we can use map, reduce & filter over an object

--- 07_day.js
+++ 07_day.js
@@ -85,3 +85,50 @@ keys.forEach(key => console.log(key));
 const values = Object.values(bookobj3);
 console.log('Values of the bookobj3 object:');
 values.forEach(value => console.log(value))
+
+//  we also can use another loop over an array and we seen it in the day 6 while revised 
+
+bookobj3.bookArray.forEach(book => {
+    console.log(book.name, book.genre, book.year);
+});
+
+let bookNames = bookobj3.bookArray.map(book => book.name);
+console.log(bookNames);
+
+let historicalBooks = bookobj3.bookArray.filter(book => book.genre === 'Historical');
+console.log(historicalBooks);
+
+// You can use Object.keys(), Object.values(), or Object.entries() in combination with forEach() to iterate over the properties of an object.
+
+Object.keys(bookobj3).forEach(key => {
+    console.log(key, bookobj3[key]);
+});
+
+const keys2 = Object.keys(bookobj3);
+console.log('Keys of the bookobj3 object:');
+keys2.forEach(key => console.log(key));
+
+Object.values(bookobj3).forEach(value => {
+    console.log(value);
+});
+
+const values2 = Object.values(bookobj3);
+console.log('Values of the bookobj3 object:');
+values2.forEach(value => console.log(value))
+  
+Object.entries(bookobj3).forEach(([key, value]) => {
+    console.log(key, value);
+});
+
+let i = 0;
+while (i < bookobj3.bookArray.length) {
+    console.log(bookobj3.bookArray[i].name);
+    i++;
+}
+
+let j = 0;
+do {
+    console.log(bookobj3.bookArray[j].name);
+    j++;
+} while (j < bookobj3.bookArray.length);
+    
